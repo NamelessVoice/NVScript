afcfa14c4c45ceb36e1dd1a0f3d6550b23efaa71
Nameless Voice <namelessvoice@gmail.com>
2022-11-03 13:02:25 +0000

    Temp: removed build commands for T2

--- Deploy-SS2.cmd
+++ Deploy-SS2.cmd
@@ -1,21 +1,21 @@
 @echo off
-set OutPath=Z:\www\nvscriptbeta\shock2
+rem set OutPath=Z:\www\nvscriptbeta\shock2
 
-echo Preparing documentation (using Git Bash tools)
-sed -i -b -e "s!/\*T2Version-Start\*/!/\*T2Version-Start!g" output/NVScript.html
-sed -i -b -e "s!/\*T2Version-End\*/!T2Version-End\*/!g" output/NVScript.html
-sed -i -b -e "s!/\*SS2Version-Start!/\*SS2Version-Start\*/!g" output/NVScript.html
-sed -i -b -e "s!SS2Version-End\*/!/\*SS2Version-End\*/!g" output/NVScript.html
-
-call PackageSource.cmd
-
-echo Creating archive
-"7z.exe" a "output\NVScript.zip" ".\output\NVScript.osm" ".\output\NVScript.html" ".\output\source" -tzip -mx9 >NUL
-
-REM Copy build output to output path
-if exist output\NVScript.osm copy output\NVScript.osm "%OutPath%"
-if exist output\NVScript.html copy output\NVScript.html "%OutPath%"
-if exist output\NVScript.zip copy output\NVScript.zip "%OutPath%\NVScript-SS2.zip"
-
-REM Copy .osm to game dirs
-if exist output\NVScript.osm copy output\NVScript.osm "X:\Games\ShockEd"
+rem echo Preparing documentation (using Git Bash tools)
+rem sed -i -b -e "s!/\*T2Version-Start\*/!/\*T2Version-Start!g" output/NVScript.html
+rem sed -i -b -e "s!/\*T2Version-End\*/!T2Version-End\*/!g" output/NVScript.html
+rem sed -i -b -e "s!/\*SS2Version-Start!/\*SS2Version-Start\*/!g" output/NVScript.html
+rem sed -i -b -e "s!SS2Version-End\*/!/\*SS2Version-End\*/!g" output/NVScript.html
+rem 
+rem call PackageSource.cmd
+rem 
+rem echo Creating archive
+rem "7z.exe" a "output\NVScript.zip" ".\output\NVScript.osm" ".\output\NVScript.html" ".\output\source" -tzip -mx9 >NUL
+rem 
+rem REM Copy build output to output path
+rem if exist output\NVScript.osm copy output\NVScript.osm "%OutPath%"
+rem if exist output\NVScript.html copy output\NVScript.html "%OutPath%"
+rem if exist output\NVScript.zip copy output\NVScript.zip "%OutPath%\NVScript-SS2.zip"
+rem 
+rem REM Copy .osm to game dirs
+rem if exist output\NVScript.osm copy output\NVScript.osm "X:\Games\ShockEd"
